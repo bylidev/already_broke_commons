7bff1f18f35e6dee20776b41b29f7edb194f58b2
nacho <igloar96@gmail.com>
2022-10-13 09:21:31 -0300

    order routing dtos

---
+++ src/main/java/dev/byli/commons/OrderRoutingEnumStatus.java
@@ -0,0 +1,11 @@
+package dev.byli.commons;
+
+public enum OrderRoutingEnumStatus {
+    NEW,
+    PARTIALLY_FILLED,
+    FILLED,
+    CANCELED,
+    PENDING_CANCEL,
+    REJECTED,
+    EXPIRED
+}
--- src/main/java/dev/byli/commons/OrderRoutingResponseDto.java
+++ src/main/java/dev/byli/commons/OrderRoutingResponseDto.java
@@ -15,9 +15,9 @@ public class OrderRoutingResponseDto {
     private String origQty;//:"10.00000000",
     private String executedQty;//:"10.00000000",
     private String cummulativeQuoteQty;//:"10.00000000",
-    private String status;//:"FILLED",
+    private OrderRoutingEnumStatus status;//:"FILLED",
     private String timeInForce;//:"GTC",
     private String type;//:"MARKET",
-    private String side;//:"SELL"
+    private OrderType side;//:"SELL"
 
 }
